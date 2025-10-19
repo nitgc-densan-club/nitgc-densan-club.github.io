8ed44e6c83e2bbebe6f9e2275d3dd7480178bd40
maru0210 <m.ryuto0210@gmail.com>
2025-10-19 10:55:38 +0900

    GitHub Pages用の設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nitgc-densan-club.github.io",
+  image: {
+    domains: ["images.microcms-assets.io"],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
